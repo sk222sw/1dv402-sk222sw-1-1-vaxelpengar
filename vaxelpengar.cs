16052c2d0769b73c0f047191f9c31b780253e2c7
Sonny Kjellberg <sonnykjellberg@gmail.com>
2014-09-09 20:35:33 +0200

    Listat ut och gjort sedelräknaren.

--- vaxelpengar.cs
+++ vaxelpengar.cs
@@ -11,11 +11,18 @@ namespace vaxelpengar
         static void Main(string[] args)
         {
             //Deklarera variabler
-            double subtotal;            
-            double roundingOffAmount;   
+            double subtotal;
+            double roundingOffAmount;
             double total;               //Ska vara uint enligt instruktioner, men det blir fel när man ska skriva in summan då.
             uint recievedAmount;        //Vad kunden ger kassören
             uint moneyBack;             //Växeln kunden får tillbaka
+            uint amountOf500Notes = 0;
+            uint amountOf100Notes = 0;
+            uint amountOf50Notes = 0;
+            uint amountOf20Notes = 0;
+            uint amountOf10Coins = 0;
+            uint amountOf5Coins = 0;
+            uint amountOf1Coins = 0;
 
             //Ta emot data från användare
             Console.Write("Ange totalsumma: ");
@@ -40,6 +47,48 @@ namespace vaxelpengar
 
 
             //Räkna ut antal sedlar med hjälp av while och %
+
+           
+
+            moneyBack = recievedAmount - (uint)total;
+
+            if (moneyBack / 500 >= 1)
+            {
+                Console.WriteLine("Antalet 500-lappar: " + moneyBack / 500);
+                moneyBack %= 500;
+            }
+            if (moneyBack / 100 >= 1)
+            {
+                Console.WriteLine("Antalet 100-lappar: " + moneyBack / 100);
+                moneyBack %= 100;
+            }
+            if (moneyBack / 50 >= 1)
+            {
+                Console.WriteLine("Antalet 50-lappar: " + moneyBack / 50);
+                moneyBack %= 50;
+            }
+            if (moneyBack / 20 >= 1)
+            {
+                Console.WriteLine("Antalet 20-lappar: " + moneyBack / 20);
+                moneyBack %= 20;
+            }
+            if (moneyBack / 10 >= 1)
+            {
+                Console.WriteLine("Antalet 10-kronor: " + moneyBack / 10);
+                moneyBack %= 10;
+            }
+            if (moneyBack / 5 >= 1)
+            {
+                Console.WriteLine("Antalet 5-kronor: " + moneyBack / 5);
+                moneyBack %= 5;
+            }
+            if (moneyBack / 1 >= 1)
+            {
+                Console.WriteLine("Antalet 1-kronor: " + moneyBack / 1);
+                moneyBack %= 1;
+            }
+
+
 
             //Skriv ut antal sedlar
         }
